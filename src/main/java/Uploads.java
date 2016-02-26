f4c8a5326ea3d8b345737055431f17e7d01bedf5
lvhkrishna <lvhkrishna@gmail.com>
2016-02-26 21:42:10 +0530

    change 6 26216

--- src/main/java/Uploads.java
+++ src/main/java/Uploads.java
@@ -48,8 +48,9 @@ public class Uploads extends HttpServlet {
  
     String filePath = request.getRequestURI().substring(request.getContextPath().length());
 	filePath = URLDecoder.decode(filePath, "UTF-8");
+	response.sendRedirect("new.jsp?path=" + filepath);
  
-    File file = new File(System.getenv("OPENSHIFT_DATA_DIR") + filePath.replace("/uploads/",""));
+    /*File file = new File(System.getenv("OPENSHIFT_DATA_DIR") + filePath.replace("/uploads/",""));
     InputStream input = new FileInputStream(file);
  
     response.setContentLength((int) file.length());
@@ -65,7 +66,7 @@ public class Uploads extends HttpServlet {
  
     out.println(System.getenv("OPENSHIFT_DATA_DIR") + filePath.replace("/uploads/",""));
 	input.close();
-    output.close();
+    output.close();*/
   }
  
   private String getFileName(Part part) {
