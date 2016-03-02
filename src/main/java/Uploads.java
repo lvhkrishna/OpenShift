4ae746b82dceb62f18f0402c4aea2738e52d477e
lvhkrishna <lvhkrishna@gmail.com>
2016-03-02 22:53:54 +0530

    Change 8 2316

--- src/main/java/Uploads.java
+++ src/main/java/Uploads.java
@@ -49,7 +49,7 @@ public class Uploads extends HttpServlet {
     String filePath = request.getRequestURI().substring(request.getContextPath().length());
 	filePath = URLDecoder.decode(filePath, "UTF-8");
 	
-	String newfilePath = System.getenv("OPENSHIFT_REPO_DIR");
+	String newfilePath = System.getenv("OPENSHIFT_DATA_DIR") + filePath.replace("/uploads/",""));
 	request.setAttribute("path", newfilePath);
 	request.getRequestDispatcher("/new.jsp").forward(request, response);
  
